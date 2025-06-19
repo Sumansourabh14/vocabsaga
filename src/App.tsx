5e298465e9188a716611148ad1295ccd78bd33ac
Suman Sourabh <sumsourabh14@gmail.com>
2025-06-19 14:28:35 +0530

    fix: removed invalid children prop from RouterProvider

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,7 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return (
-    <RouterProvider router={router}>
-      <Home />
-    </RouterProvider>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
