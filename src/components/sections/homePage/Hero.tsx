2be675ec6bd07d1541e4a4c12d81103a0523b684
Suman Sourabh <sumsourabh14@gmail.com>
2025-07-20 00:28:43 +0530

    feat: added shadcn and hero of home page

---
+++ src/components/sections/homePage/Hero.tsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <section className="px-4 py-16 text-center max-w-2xl mx-auto">
+      <h1 className="text-3xl md:text-5xl lg:text-7xl 2xl:text-8xl font-bold tracking-tight mb-4">
+        <span className="text-[#1b7a1b]">Vocab</span> Saga
+      </h1>
+      <p className="text-base md:text-lg">
+        Build your own vocabulary, one word at a time.
+      </p>
+    </section>
+  );
+};
+
+export default Hero;
