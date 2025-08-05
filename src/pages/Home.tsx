a4dcf0d9faebdd468ce201590d653426657c3408
Suman Sourabh <sumsourabh14@gmail.com>
2025-08-06 00:55:14 +0530

    updated hero

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,4 @@
 import Hero from "@/components/sections/homePage/Hero";
-import WordOfDay from "@/components/sections/WordOfDay";
-import { Separator } from "@/components/ui/separator";
-import WordInput from "@/components/WordInput";
 
 const Home = () => {
   return (
@@ -9,11 +6,6 @@ const Home = () => {
       <main className="max-w-[1300px] mx-auto">
         <section>
           <Hero />
-          <WordInput />
-
-          <Separator />
-
-          <WordOfDay />
         </section>
       </main>
     </>
