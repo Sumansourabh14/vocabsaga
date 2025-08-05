de8d5a9a03e90e882fe14eeeb81e91ce36e0a67b
Suman Sourabh <sumsourabh14@gmail.com>
2025-08-06 01:34:49 +0530

    removed hard coded bg on root layout

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -4,7 +4,7 @@ import Navbar from "./Navbar";
 
 export default function RootLayout() {
   return (
-    <div className="flex flex-col bg-white dark:bg-black">
+    <div className="flex flex-col">
       <Navbar />
       <main className="flex-1 px-4 py-6">
         <Outlet />
