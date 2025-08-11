93e28ee3b32904b1fed72cd3a79e010d7486bc9f
Suman Sourabh <sumsourabh14@gmail.com>
2025-08-11 17:14:31 +0530

    added launched on peerlist badge on hero

--- src/components/sections/homePage/Hero.tsx
+++ src/components/sections/homePage/Hero.tsx
@@ -1,18 +1,37 @@
+import { useTheme } from "@/context/theme-provider";
+
 const Hero = () => {
+  const { theme } = useTheme();
+
   return (
-    <section className="max-w-4xl px-4 py-20 md:py-28 text-center mx-auto space-y-4">
+    <section className="max-w-4xl px-4 py-20 md:py-28 text-center mx-auto space-y-4 flex flex-col items-center">
       <h1 className="max-w-3xl text-5xl md:text-7xl xl:text-8xl font-bold tracking-tight mx-auto">
-        Not your average <span className="text-[#1b7a1b]">vocabulary</span> app.
+        Not your average <span className="text-[#1b7a1b]">vocabulary</span> app
       </h1>
-      <p className="text-2xl mt-8">
-        Learn through stories, not with single words.
+      <p className="text-xl mt-4 text-muted-foreground">
+        Learn words through passages, not with single words.
       </p>
       <a
         href="/story"
-        className="inline-block px-6 py-3 mt-4 bg-[#1b7a1b] text-white text-lg font-semibold rounded-lg hover:bg-green-800 transition"
+        className="inline-block px-6 py-3 mt-4 mb-8 bg-[#1b7a1b] text-white text-lg font-semibold rounded-lg hover:bg-green-800 transition"
       >
         Start Learning
       </a>
+      <div>
+        <a
+          href="https://peerlist.io/sumansourabh/project/vocabsaga"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={`https://peerlist.io/api/v1/projects/embed/PRJHEOGEDBQRRQKPR2R87KAQE9QO7G?showUpvote=false&theme=${
+              theme === "light" ? "light" : "dark"
+            }`}
+            alt="Vocabsaga"
+            style={{ width: "14rem" }}
+          />
+        </a>
+      </div>
     </section>
   );
 };
